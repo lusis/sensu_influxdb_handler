d4b86769d01aa57306bf99a3909360e6a3a706ef
John E. Vincent <lusis.org+github.com@gmail.com>
2014-02-04 14:30:48 -0600

    convert to a proper extension

--- influx.rb
+++ influx.rb
@@ -1,19 +1,56 @@
-#!/usr/bin/env ruby
 require 'rubygems' if RUBY_VERSION < '1.9.0'
-require 'sensu-handler'
 require 'influxdb'
+require 'timeout'
 
-class Influx < Sensu::Handler
-  def filter; end
+module Sensu::Extension
 
-  def handle
-    @influxdb = InfluxDB::Client.new settings['influx']['database'], :host => settings['influx']['host'], :port => settings['influx']['port'], :username => settings['influx']['user'], :password => settings['influx']['password']
-    @event['check']['output'].split("\n").each do |line|
-      n, v, _ = line.split(/\s+/)
-      if settings['influx']['strip_metric']
-        n.gsub!(/^.*#{settings['influx']['strip_metric']}\.(.*$)/, '\1')
-      end
-      @influxdb.write_point(@event['check']['name'], {:host => @event['client']['name'], :metric => n, :value => v, :duration => @event['check']['duration']})
+  class Influx < Handler
+
+    def name
+      'influx'
     end
+
+    def description
+      'outputs metrics to InfluxDB'
+    end
+
+    def post_init
+      @influxdb = InfluxDB::Client.new settings['influx']['database'], :host => settings['influx']['host'], :port => settings['influx']['port'], :username => settings['influx']['user'], :password => settings['influx']['password']
+      @timeout = @settings['influx']['timeout'] || 15
+    end
+
+    def run(event)
+      begin
+        event = Oj.load(event)
+        host = event[:client][:name]
+        series = event[:check][:name]
+        timestamp = event[:check][:issued]
+        duration = event[:check][:duration]
+        output = event[:check][:output]
+      rescue => e
+        @logger.error("InfluxDB: Error setting up event object - #{e.backtrace.to_s}")
+      end
+
+      begin
+        points = []
+        output.split(/\n/).each do |line|
+          @logger.debug("Parsing line: #{line}")
+	  k,v,t = line.split(/\s+/)
+          k.gsub!(/^.*#{@settings['influx']['strip_metric']}\.(.*$)/, '\1') if @settings['influx']['strip_metric']
+          points << {:time => t.to_f, :host => host, :metric => k, :value => v}
+        end
+      rescue => e
+        @logger.error("InfluxDB: Error parsing output lines - #{e.backtrace.to_s}")
+        @logger.error("InfluxDB: #{output}")
+      end
+
+      begin
+        @influxdb.write_point(series, points, true)
+      rescue => e
+        @logger.error("InfluxDB: Error posting event - #{e.backtrace.to_s}")
+      end
+      yield("InfluxDB: Handler finished", 0)
+    end
+
   end
 end
